5a2dfc84e9b627a54670fb8ac8a81386c5ba63bf
ShirleyDanielaPerezMancilla <shirleydp1109@gmail.com>
2023-02-08 20:24:50 -0500

    Operadores aritméticos

---
+++ main.js
@@ -0,0 +1,32 @@
+let num1 = 10, num2 = 3;
+let resultado;
+
+//suma
+resultado = num1 + num2;
+console.log(`${num1} + ${num2} = ${resultado}`);
+
+//resta
+resultado = num1 - num2;
+console.log(`${num1} - ${num2} = ${resultado}`);
+
+//multiplicación
+resultado = num1 * num2;
+console.log(`${num1} * ${num2} = ${resultado}`);
+
+//división
+resultado = num1 / num2;
+console.log(`${num1} / ${num2} = ${resultado}`);
+
+//Exponenciación
+resultado = num1 ** num2;
+console.log(`${num1} ^ ${num2} = ${resultado}`);
+
+//Residuo (%)
+resultado = num1 % num2;
+console.log(`${num1} % ${num2} = ${resultado}`);
+
+//Unary negation
+console.log(`${-num1} , ${-num2} `);
+
+//Unary plus
+console.log(`${+num1}  ${+num2} `);
