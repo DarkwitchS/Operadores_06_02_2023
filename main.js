6c6cec5ab1aace2c2e27a1a49271c81fa628adb8
ShirleyDanielaPerezMancilla <shirleydp1109@gmail.com>
2023-02-14 22:48:23 -0500

    Operador && AND

--- main.js
+++ main.js
@@ -1,41 +1,10 @@
+let a = confirm("Variable A\n\t\tSelecciona Aceptar si deseas el booleano True\n\t\tSelecciona Cancelar si deseas el booleano False");
+let b = confirm("Variable B\n\t\tSelecciona Aceptar si deseas el booleano True\n\t\tSelecciona Cancelar si deseas el booleano False");
 
-//Equality (a == b)
-let numeroa = 8, numerob = 8;
-let resultado = numeroa == numerob;
-console.log(`${numeroa} == ${numerob} ? ${resultado}`);
-
-/* Inequality (a != b)*/
-numeroa = 6, numerob = "6"
-resultado = numeroa != numerob;
-console.log(`${numeroa} != '${numerob}' ? ${resultado}`);
-
-/* Identity (a === b) */
-numeroa = 10 , numerob = "10"
-resultado = numeroa === numerob;
-console.log(`${numeroa} === '${numerob}' ? ${resultado}`);
-
-/* Non-identity (a !== b)*/
-numeroa = 86 , numerob = "86"
-resultado = numeroa !== numerob;
-console.log(`${numeroa} !== '${numerob}' ? ${resultado}`);
-
-/* Greater than (a > b) */
-numeroa = 10 , numerob = 36
-resultado = numeroa > numerob;
-console.log(`${numeroa} > ${numerob} ? ${resultado}`);
-
-/* Greater than or equal (a >= b)*/
-numeroa = 10 , numerob = 2
-resultado = numeroa >= numerob;
-console.log(`${numeroa} >= ${numerob} ? ${resultado}`);
-
-/* Less than (a < b) */
-numeroa = 10 , numerob = 87
-resultado = numeroa < numerob;
-console.log(`${numeroa} < ${numerob} ? ${resultado}`);
-
-/* Less than or equal (a <= b) */
-numeroa = 10 , numerob = "10"
-resultado = numeroa <= numerob;
-console.log(`${numeroa} <= '${numerob}' ? ${resultado}`);
+let cuerpo = document.querySelector("#myAND");
+cuerpo.insertAdjacentHTML("beforeend", ` <tr>
+                                            <td>${a}</td>
+                                            <td>${b}</td>
+                                            <td>${a && b}</td>
+                                        </tr>`);
 
